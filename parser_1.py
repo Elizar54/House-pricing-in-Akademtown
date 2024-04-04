26a70594fbd1b17c4663a7387f53b9abd8ae0834
Elizar54 <y.elizarev@g.nsu.ru>
2024-04-04 23:40:45 +0700

    update

--- parser_1.py
+++ parser_1.py
@@ -33,11 +33,11 @@ cur = conn.cursor()
 
 URL = 'https://novosibirsk.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&region=4897&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1&room7=1&room9=1'
 link_list = []
-p = 163
+p = 516
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=set_chrome_options())
 wait = WebDriverWait(driver, 10)
 
-while len(link_list) < 10000:
+while len(link_list) < 5000:
     if p % 10 == 0:
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=set_chrome_options())
     driver.get(URL)
@@ -49,7 +49,7 @@ while len(link_list) < 10000:
 
 
 print('ссылки выгружены', p)
-id = 4515
+id = 14449
 
 columns = set(('id', 'Общая площадь', 'Этаж', 'Год сдачи', 'Дом', 'Отделка', 'price', 'metro', \
                 'address', 'Жилая площадь', 'Площадь кухни', 'Высота потолков', 'Балкон/лоджия', \
@@ -60,6 +60,7 @@ for link in link_list:
     if id % 100 == 0:
         conn = psycopg2.connect(host='ep-black-pond-a2ydwdvs.eu-central-1.aws.neon.tech', database='Akademdb', user='Elizar54', password='XUpC1QOnGvA4')
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=set_chrome_options())
+
     try:    
         driver.get(link)
         flat = {}
@@ -67,63 +68,63 @@ for link in link_list:
 
         flat['id'] = id
 
-        elements = driver.find_elements(By.CLASS_NAME, 'a10a3f92e9--item--Jp5Qv')
-        
-        if elements == []:
-            descript = driver.find_element(By.CLASS_NAME, 'a10a3f92e9--group--K5ZqN')
-            elements = descript.find_elements(By.CLASS_NAME, 'a10a3f92e9--item--qJhdR')
-        
-        price = driver.find_element(By.CLASS_NAME, 'a10a3f92e9--amount--ON6i1')
-
-        home_elems = driver.find_elements(By.CLASS_NAME, 'a10a3f92e9--item--qJhdR')
-        
-        if home_elems != []:
-            for elem in home_elems:
-                elem_lst = elem.text.split('\n')
-                flat[elem_lst[0]] = elem_lst[1]
-
         address = driver.find_elements(By.CLASS_NAME, 'a10a3f92e9--address--SMU25')
-
-        try:
-            metro = driver.find_element(By.CLASS_NAME, 'a10a3f92e9--undergrounds--sGE99')
-        except:
-            pass
-
         for elem in address:
             address_str += elem.text + ' '
 
-        for elem in elements:
-            elem_lst = elem.text.split('\n')
-            flat[elem_lst[0]] = elem_lst[1]
-        
-        flat['price'] = price.text
-        flat['metro'] = metro.text
-        flat['address'] = address_str
+        if not('Кировский' in address_str or 'Ленинский' in address_str):
+            elements = driver.find_elements(By.CLASS_NAME, 'a10a3f92e9--item--Jp5Qv')
+            
+            if elements == []:
+                descript = driver.find_element(By.CLASS_NAME, 'a10a3f92e9--group--K5ZqN')
+                elements = descript.find_elements(By.CLASS_NAME, 'a10a3f92e9--item--qJhdR')
+            
+            price = driver.find_element(By.CLASS_NAME, 'a10a3f92e9--amount--ON6i1')
 
-        
-        
-        if 'Тип жилья' in set(flat.keys()): 
-            flat['Дом'] = flat['Тип жилья'] 
-            del flat['Тип жилья']
+            home_elems = driver.find_elements(By.CLASS_NAME, 'a10a3f92e9--item--qJhdR')
+            
+            if home_elems != []:
+                for elem in home_elems:
+                    elem_lst = elem.text.split('\n')
+                    flat[elem_lst[0]] = elem_lst[1]
 
-        for key in columns - set(flat.keys()):
-            flat[key] = 'None' 
+            try:
+                metro = driver.find_element(By.CLASS_NAME, 'a10a3f92e9--undergrounds--sGE99')
+            except:
+                pass
 
-        if flat['metro'] == '':
-            flat['metro'] = 'None'
+            for elem in elements:
+                elem_lst = elem.text.split('\n')
+                flat[elem_lst[0]] = elem_lst[1]
+            
+            flat['price'] = price.text
+            flat['metro'] = metro.text
+            flat['address'] = address_str
 
-        if 'Год постройки' in set(flat.keys()): del flat['Год постройки']
+            
+            
+            if 'Тип жилья' in set(flat.keys()): 
+                flat['Дом'] = flat['Тип жилья'] 
+                del flat['Тип жилья']
 
-        id += 1
-        
-        with conn.cursor() as curs:
-            tuple_insert = (flat['id'], flat['Общая площадь'], flat['Этаж'], flat['Год сдачи'], flat['Дом'], flat['Отделка'],\
-                                        flat['price'], flat['metro'], flat['address'], flat['Жилая площадь'], flat['Площадь кухни'], \
-                                        flat['Высота потолков'], flat['Санузел'], flat['Балкон/лоджия'], flat['Вид из окон'], flat['Тип перекрытий'], \
-                                        flat['Мусоропровод'], flat['Тип дома'], flat['Отопление'], flat['Подъезды'], flat['Аварийность'], flat['Количество лифтов'])
-            curs.execute("INSERT INTO flats_new (id, total_square, floor, year, rented, decor, price, metro, address, living_sq, kitchen_sq, ceil_height, \
-                                    toilets, balcony, window_view, barriers, trash, home_type, warm, entrance, warning, elevators) VALUES (%s, %s, %s, \
-                                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", tuple_insert)
-            conn.commit()
+            for key in columns - set(flat.keys()):
+                flat[key] = 'None' 
+
+            if flat['metro'] == '':
+                flat['metro'] = 'None'
+
+            if 'Год постройки' in set(flat.keys()): del flat['Год постройки']
+
+            id += 1
+            
+            with conn.cursor() as curs:
+                tuple_insert = (flat['id'], flat['Общая площадь'], flat['Этаж'], flat['Год сдачи'], flat['Дом'], flat['Отделка'],\
+                                            flat['price'], flat['metro'], flat['address'], flat['Жилая площадь'], flat['Площадь кухни'], \
+                                            flat['Высота потолков'], flat['Санузел'], flat['Балкон/лоджия'], flat['Вид из окон'], flat['Тип перекрытий'], \
+                                            flat['Мусоропровод'], flat['Тип дома'], flat['Отопление'], flat['Подъезды'], flat['Аварийность'], flat['Количество лифтов'])
+                curs.execute("INSERT INTO flats_new (id, total_square, floor, year, rented, decor, price, metro, address, living_sq, kitchen_sq, ceil_height, \
+                                        toilets, balcony, window_view, barriers, trash, home_type, warm, entrance, warning, elevators) VALUES (%s, %s, %s, \
+                                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", tuple_insert)
+                conn.commit()
     except:
         pass
